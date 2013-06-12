79dc845a223b9821e880ff324908b65a67f4f810
dtripathy10 <dtripathy10@gmail.com>
2013-06-12 15:37:23 +0530

    ff

--- footer.php
+++ footer.php
@@ -32,19 +32,11 @@ function getFooter($id) {
     </ul>
   </div>
 </div>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-37587264-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>  </body>
+	<?php
+include_once("util/system.php");
+getgoogleanalytics();
+?>
+</body>
 </html>
 <?php
 }
