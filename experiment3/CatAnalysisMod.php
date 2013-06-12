1786b544b799c300c063e13fa0d802a59f40e8f4
rsharma07 <ritesh_sit1@yahoo.com>
2013-06-12 11:58:00 +0530

    header modified

--- experiment3/CatAnalysisMod.php
+++ experiment3/CatAnalysisMod.php
@@ -1,7 +1,7 @@
  <?php
 include_once("../util/system.php");
 include_once("header.php");
-getHeader(4);
+getHeader(4,"Catagory Analysis","Trip Generatrion");
 session_start();
 $UploadFile = $_SESSION['user'];
 $folder = USER_ROOT."/".$UploadFile."/Experiment3";
