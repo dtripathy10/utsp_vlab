77e9a21ca653882856480aa7d16fee09303238fa
dtripathy10 <dtripathy10@gmail.com>
2013-06-15 18:56:00 +0530

    report.php is resolved

--- login/report.php
+++ login/report.php
@@ -21,51 +21,54 @@ getHeader(9, "Repoort | UTSP VLab");
         session_start();
         $root_dir = USER_ROOT . '/' . $_SESSION['user'];
 
-        function sub_string($mystring,$findname) {
-			$pos = strpos($mystring, $findname);
-			return $pos;
+        function sub_string($mystring, $findname) {
+            $pos = strpos($mystring, $findname);
+            return $pos;
         }
-        function exp_name($a) {	
-        if(sub_string($a,"Experiment1")) {
-        	return "Volume, Speed and Delay Study at Intersection";
-        }else if(sub_string($a,"Experiment2")) {
-        	return "Trip Generation:Regression Analysis";
-        }else if(sub_string($a,"Experiment3")) {
-        	return "Trip Generation:Category Analysis";
-        }else if(sub_string($a,"Experiment4")) {
-        	return "Trip Distribution:Growth Factor Distribution Model";
-        }else if(sub_string($a,"Experiment5")) {
-        	return "Trip Distribution:Singly constrained Gravity Model";
-        }else if(sub_string($a,"Experiment6")) {
-        	return "Trip Distribution:Doubly constrained Gravity Model";
-        }else if(sub_string($a,"Experiment7")) {
-        	return "Trip Distribution:Calibration of Singly Constrained Gravity Model";
-        }else if(sub_string($a,"Experiment8")) {
-        	return "Trip Distribution:Calibration of Doubly Constrained Gravity Model";
-        }else if(sub_string($a,"Experiment9")) {
-        	return "Mode Split";
-        }else if(sub_string($a,"Experiment10")) {
-        	return "Trip Assignment:All or Nothing (AON) Assignment";
-        }else if(sub_string($a,"Experiment11")) {
-        	return "Trip Assignment:User Equilibrium Assignment";
-        }else if(sub_string($a,"Experiment12")) {
-        	return "Trip Assignment:System Optimal Assignment";
-        }else {
-        	return "error";
+
+        function exp_name($a) {
+            if (sub_string($a, "Experiment1")) {
+                if (sub_string($a, "Experiment10")) {
+                    return "Trip Assignment:All or Nothing (AON) Assignment";
+                } else if (sub_string($a, "Experiment11")) {
+                    return "Trip Assignment:User Equilibrium Assignment";
+                } else if (sub_string($a, "Experiment12")) {
+                    return "Trip Assignment:System Optimal Assignment";
+                }
+                return "Volume, Speed and Delay Study at Intersection";
+            } else if (sub_string($a, "Experiment2")) {
+                return "Trip Generation:Regression Analysis";
+            } else if (sub_string($a, "Experiment3")) {
+                return "Trip Generation:Category Analysis";
+            } else if (sub_string($a, "Experiment4")) {
+                return "Trip Distribution:Growth Factor Distribution Model";
+            } else if (sub_string($a, "Experiment5")) {
+                return "Trip Distribution:Singly constrained Gravity Model";
+            } else if (sub_string($a, "Experiment6")) {
+                return "Trip Distribution:Doubly constrained Gravity Model";
+            } else if (sub_string($a, "Experiment7")) {
+                return "Trip Distribution:Calibration of Singly Constrained Gravity Model";
+            } else if (sub_string($a, "Experiment8")) {
+                return "Trip Distribution:Calibration of Doubly Constrained Gravity Model";
+            } else if (sub_string($a, "Experiment9")) {
+                return "Mode Split";
+            } else {
+                return "error";
+            }
         }
-        	
-        }
+
         $__counter = 0;
+
         function traverse($dir) {
-        global $__counter;
+            global $__counter;
             $results_array = array(".", "..");
             if (!is_dir($dir)) {
-            	
-            	$data = $dir."/".basename($dir);
-            	$exp_name = exp_name($dir);
-            	$file_name = basename($dir);
-            	
-            	echo "<tr><td>".++$__counter."</td><td>".$exp_name."</td><td><a href=".$dir." target='_blank'>".$file_name."</a></td></tr>";
+
+                $data = $dir . "/" . basename($dir);
+                $exp_name = exp_name($dir);
+                $file_name = basename($dir);
+
+                echo "<tr><td>" . ++$__counter . "</td><td>" . $exp_name . "</td><td><a href=" . $dir . " target='_blank'>" . $file_name . "</a></td></tr>";
                 return;
             } else {
                 $results_array = scandir($dir);
@@ -77,7 +80,7 @@ getHeader(9, "Repoort | UTSP VLab");
                 }
                 if (!is_dir($value)) {
                     //echo $dir."/".basename($value) . "<br/>";
-                    traverse($dir."/".basename($value));
+                    traverse($dir . "/" . basename($value));
                 } else {
                     //echo $dir."/".basename($value) . "<br/>";
                 }
